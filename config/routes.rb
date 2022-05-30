409f3b8f3918c684d65455a8145bae1b18d98048
Edward Beesley <edwardjbeesley@gmail.com>
2022-05-30 14:52:23 +0100

    added movies controller and empty methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/search', to: 'movies#search'
-  get 'movies', to: 'movies#searchresults'
+  get 'movies', to: 'movies#search_results'
 
   resources :movie_bookmarks, only: [ :index, :show, :create, :destroy ]
 end
