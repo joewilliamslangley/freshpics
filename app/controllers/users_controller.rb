701b8cc82b607bca4852d4ae93156227a1cca5df
Jenny Higgins <higgjd@gmail.com>
2022-06-07 21:42:48 +0100

    added basic content to the user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @movie_bookmarks = MovieBookmark.where(user: params[:id])
+    @movies = @user.movies
   end
 end
