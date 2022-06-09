09e50ba2f8b265dcfd9647284e2b3187e89854b6
Darren Rounce <drounce86@gmail.com>
2022-06-09 21:12:05 +0100

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/search', to: 'movies#search'
   get 'movies', to: 'movies#search_results'
   get 'movies/:id', to: 'movies#show', as: :movie
+
   resources :movie_bookmarks, only: [ :index, :show, :create, :destroy ]
   resources :users, only: [:show]
 end
