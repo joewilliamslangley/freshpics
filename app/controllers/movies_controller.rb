332a182e28647e6465128e712350c0fa5aca37ce
Joe Williams-Langley <joewilliamslangley@gmail.com>
2022-06-09 21:31:07 +0100

    created search page and very basic search form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,10 @@
 class MoviesController < ApplicationController
   def search
-
+    if params[:query].present?
+      @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
   end
 
   def search_results
