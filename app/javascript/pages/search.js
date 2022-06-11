bc948ea5109c60226614258f7ea35d267add128d
Joe Williams-Langley <joewilliamslangley@gmail.com>
2022-06-11 16:41:15 +0100

    added basic javascript functionality to search page

--- app/javascript/pages/search.js
+++ app/javascript/pages/search.js
@@ -1,1 +1,19 @@
-console.log('Hi Juan')
+let number = 1;
+let question = `question-${number}`;
+let btn = document.getElementById(`btn-${number}`);
+const addEventListenerToButtons = ((btn) => {
+  btn.addEventListener('click', (event) => {
+    number += 1;
+    if (number < 5) {
+      event.preventDefault();
+      nextDiv = document.getElementById(`question-${number}`);
+      console.log(nextDiv);
+      btn = document.getElementById(`btn-${number}`);
+      addEventListenerToButtons(btn);
+    }
+  });
+});
+
+if (btn) {
+  addEventListenerToButtons(btn);
+}
