e7873d6a6d2e235fea3174d0fc6e1df23f446c4d
Jenny Higgins <higgjd@gmail.com>
2022-06-11 12:35:01 +0200

    added the destroy function and link to destroy

--- app/controllers/movie_bookmarks_controller.rb
+++ app/controllers/movie_bookmarks_controller.rb
@@ -2,4 +2,11 @@ class MovieBookmarksController < ApplicationController
   def create
     @movie = Movie.current_user.create!(movie_params)
   end
+
+  def destroy
+    @movie_bookmark = MovieBookmark.find(params[:id])
+    @user = current_user
+    @movie_bookmark.destroy
+    redirect_to user_path(@user)
+  end
 end
