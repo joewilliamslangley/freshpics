24472145739b6ce4149fa0a75a3e2b223b85acaa
Edward Beesley <edwardjbeesley@gmail.com>
2022-06-12 17:34:43 +0100

    added watchlist type to form to create moviebookmark

--- app/controllers/movie_bookmarks_controller.rb
+++ app/controllers/movie_bookmarks_controller.rb
@@ -6,7 +6,7 @@ class MovieBookmarksController < ApplicationController
   def create
     @movies = Movie.all
     @movie = Movie.find(params[:movie_bookmark][:movie])
-    @movie_bookmark = MovieBookmark.new
+    @movie_bookmark = MovieBookmark.new(movie_bookmark_params)
     @movie_bookmark.user = current_user
     @movie_bookmark.movie = @movie
     @movie_bookmark.save!
@@ -19,4 +19,11 @@ class MovieBookmarksController < ApplicationController
     @movie_bookmark.destroy
     redirect_to user_path(@user)
   end
+
+  private
+
+  def movie_bookmark_params
+    params.require(:movie_bookmark).permit(:bookmark_type)
+  end
+
 end
