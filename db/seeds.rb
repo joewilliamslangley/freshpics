01680ea4a05a559772dc35ff24a7be8e59f9e5ad
Edward Beesley <edwardjbeesley@gmail.com>
2022-06-15 21:53:23 +0100

    final method written to seed movies currently in Heroku database

--- db/seeds.rb
+++ db/seeds.rb
@@ -116,6 +116,7 @@ def add_streaming_data(min, max)
         create_platform_bookmark(movie, platform, motn_data) if motn_data["streamingInfo"][platform]
       end
       puts "#{movie.title} saved!"
+      sleep 1
     end
   end
 end
@@ -129,4 +130,4 @@ def add_motn_data(min, max, last)
   end
 end
 
-add_streaming_data(1, 100)
+add_streaming_data(101, 6857)
