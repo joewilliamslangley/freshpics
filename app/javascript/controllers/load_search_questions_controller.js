9cdb7ad827c540f49dae726d069240c96f1a870c
Joe Williams-Langley <joewilliamslangley@gmail.com>
2022-06-17 11:52:36 +0100

    commented out old search.js file and created working load function in controller

--- app/javascript/controllers/load_search_questions_controller.js
+++ app/javascript/controllers/load_search_questions_controller.js
@@ -3,7 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["button"]
 
-  greet() {
-    console.log(`Hello, the element is:${this.buttonTarget.id}`)
+  load(event) {
+    const id = this.element.id;
+    const idNumber = parseInt(id.substr(id.length - 1));
+
+    event.preventDefault();
+    this.element.classList.add("hidden");
+
+    const nextQuestion = document.getElementById(`question-${idNumber + 1}`);
+    nextQuestion.classList.remove("hidden");
+    nextQuestion.classList.add("fade-in");
   }
 }
