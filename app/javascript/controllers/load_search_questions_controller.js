3104d5f9350d8c2f13883449572333d5a1ede71a
Joe Williams-Langley <joewilliamslangley@gmail.com>
2022-06-18 15:57:20 +0100

    refactored controller to make code DRY and added font awesome icons for arrows

--- app/javascript/controllers/load_search_questions_controller.js
+++ app/javascript/controllers/load_search_questions_controller.js
@@ -4,29 +4,25 @@ let num = 0;
 export default class extends Controller {
   static targets = ["button", "question", "back", "forward"];
 
-  nextQuestion(event) {
+  handleLoading(event, addNum) {
     event.preventDefault();
-
     this.questionTargets[num].classList.add("hidden");
-    this.questionTargets[num + 1].classList.remove("hidden");
-    this.questionTargets[num + 1].classList.add("fade-in");
+    this.questionTargets[num + addNum].classList.remove("hidden");
+    this.questionTargets[num + addNum].classList.add("fade-in");
+    num += addNum;
+  }
 
-    if (num == 0) this.backTarget.classList.remove("hidden");
-    if (num == 2) this.forwardTarget.classList.add("hidden");
+  nextQuestion(event) {
+    this.handleLoading(event, 1)
 
-    num += 1;
+    if (num == 1) this.backTarget.classList.remove("hidden");
+    if (num == 3) this.forwardTarget.classList.add("hidden");
   }
 
   previousQuestion(event) {
-    event.preventDefault();
+    this.handleLoading(event, -1)
 
-    this.questionTargets[num].classList.add("hidden");
-    this.questionTargets[num - 1].classList.remove("hidden");
-    this.questionTargets[num - 1].classList.add("fade-in");
-
-    if (num == 1) this.backTarget.classList.add("hidden");
-    if (num == 3) this.forwardTarget.classList.remove("hidden");
-
-    num -= 1;
+    if (num == 0) this.backTarget.classList.add("hidden");
+    if (num == 2) this.forwardTarget.classList.remove("hidden");
   }
 }
