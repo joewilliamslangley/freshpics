ad7d0c87550103238bb89d0145a7dcf1f376d0aa
Jenny Higgins <higgjd@gmail.com>
2022-06-20 15:09:32 +0100

    added data target and tested

--- app/javascript/controllers/range_slider_value_controller.js
+++ app/javascript/controllers/range_slider_value_controller.js
@@ -3,13 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 const slider = document.getElementById("scoreRange");
 const output = document.getElementById("value-output");
 
+
 export default class extends Controller {
+  static targets = [ "valueOutput", "scoreRange" ]
+
   connect() {
     // console.log("The 'range slider value' controller is now loaded!")
   }
 
   showValue(event) {
     console.log(event)
+    this.valueOutputTarget.innerHTML = "Number"
     // output.innerHTML = slider.value;
     // slider.oninput = function() {
     // output.innerHTML = this.value;
