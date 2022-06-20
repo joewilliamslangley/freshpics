a334b18f18e4de2fbbfaf8aecb5df8620301ae6b
Edward Beesley <edwardjbeesley@gmail.com>
2022-06-20 16:46:03 +0100

    added multiple fonts and background images and set style for home page to randomly alternate between 12 different designs, based on classic films

---
+++ app/javascript/controllers/home_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = []
+
+  connect() {
+    // console.log(document.querySelector('.home-title'))
+    // document.querySelector(".wrapper").style.backgroundImage = `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('<%= image_path 'https://www.themoviedb.org/t/p/original/umaoScaCsfl9g2dyrXrvMPp59Q7.jpg'%>')"`;
+    // document.querySelector('.home-subtitle').style.fontFamily = `alien-bold`
+  }
+
+
+}
