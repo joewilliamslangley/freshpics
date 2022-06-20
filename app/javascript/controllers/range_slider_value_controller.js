94b531955d9ca350728330f55c961d96e8a85461
Jenny Higgins <higgjd@gmail.com>
2022-06-20 14:47:10 +0100

    added the showValue method to test

--- app/javascript/controllers/range_slider_value_controller.js
+++ app/javascript/controllers/range_slider_value_controller.js
@@ -1,7 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
+const slider = document.getElementById("scoreRange");
+const output = document.getElementById("value-output");
+
 export default class extends Controller {
   connect() {
-    console.log("The 'range slider value' controller is now loaded!")
+    // console.log("The 'range slider value' controller is now loaded!")
   }
+
+  showValue(event) {
+    console.log(event)
+    // output.innerHTML = slider.value;
+    // slider.oninput = function() {
+    // output.innerHTML = this.value;
+  }
+
+
 }
