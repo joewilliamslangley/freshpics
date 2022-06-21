c08d3ed705453839b561ee534ecf428ef1506e60
Jenny Higgins <higgjd@gmail.com>
2022-06-21 21:39:11 +0100

    added the time slider and separated from the score slider

--- app/javascript/controllers/range_slider_value_controller.js
+++ app/javascript/controllers/range_slider_value_controller.js
@@ -11,11 +11,9 @@ export default class extends Controller {
     // console.log("The 'range slider value' controller is now loaded!")
   }
 
-  showValue(event) {
+  showScore(event) {
     // console.log(event)
-
-    // Update the current slider value (each time you drag the slider handle)
-    this.valueOutputTarget.innerHTML = this.scoreRangeTarget.value
+    this.valueOutputTarget.innerHTML = `${this.scoreRangeTarget.value}%`
 
     // JAVASCRIPT CODE
     // output.innerHTML = slider.value; // Display the default slider value
@@ -25,6 +23,22 @@ export default class extends Controller {
     //   output.innerHTML = this.value;
     }
 
+  showTime(event) {
+    // console.log(event)
+    if (this.scoreRangeTarget.value > 179) {
+      this.valueOutputTarget.innerHTML = "No limit";
+    } else {
+      this.valueOutputTarget.innerHTML = `${this.scoreRangeTarget.value} mins`
+    }
+    // Update the current slider value (each time you drag the slider handle)
+    // this.valueOutputTarget.innerHTML = this.scoreRangeTarget.value
 
+    // JAVASCRIPT CODE
+    // output.innerHTML = slider.value; // Display the default slider value
+
+    // // Update the current slider value (each time you drag the slider handle)
+    // slider.oninput = function() {
+    //   output.innerHTML = this.value;
+    }
 
 }
