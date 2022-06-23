f6afd6048a1676f1f0c21ad12f1697b5bcef3577
Edward Beesley <edwardjbeesley@gmail.com>
2022-06-23 19:32:31 +0100

    fixed typo on job

--- app/jobs/update_ratings_job.rb
+++ app/jobs/update_ratings_job.rb
@@ -13,10 +13,10 @@ class UpdateRatingsJob < ApplicationJob
       movie_data = get_omdb_ratings(movie.imdb_id)
       next if movie_data == "Error"
 
-      movie.imdb_rating = (movie_data.imDbRating.to_f * 10).to_i if result.imDbRating != "N/A"
-      movie.metacritic_rating = movie_data.Metascore.to_i if result.Metascore != "N/A"
+      movie.imdb_rating = (movie_data.imDbRating.to_f * 10).to_i if movie_data.imDbRating != "N/A"
+      movie.metacritic_rating = movie_data.Metascore.to_i if movie_data.Metascore != "N/A"
       ratings.each do |rating|
-        movie.rotten_tomatoes_rating = rating.Value.gsub(/[^0-9]/, '').to_i if rating.Source == "Rotten Tomatoes"
+        movie.rotten_tomatoes_rating = movie_data.Value.gsub(/[^0-9]/, '').to_i if movie_data.Source == "Rotten Tomatoes"
       end
       if movie_data.Language.split(", ")[0] == "English"
         movie.english = true
@@ -30,7 +30,7 @@ class UpdateRatingsJob < ApplicationJob
   private
 
   def get_omdb_ratings(movie_id)
-    url = "https://www.omdbapi.com/?i=#{movie_id}&apikey=98973290"
+    url = "https://www.omdbapi.com/?i=#{movie_id}&apikey=#{ENV['OMDB_KEY']}"
     serialized = URI.open(url).read
     JSON.parse(serialized, object_class: OpenStruct)
   rescue JSON::ParserError
