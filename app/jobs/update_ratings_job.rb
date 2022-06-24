33e58f3a6e8e5847abc2748352bfb09aaac64154
Edward Beesley <edwardjbeesley@gmail.com>
2022-06-24 09:24:23 +0100

    hopefully fixed two bugs on the job

--- app/jobs/update_ratings_job.rb
+++ app/jobs/update_ratings_job.rb
@@ -8,15 +8,17 @@ class UpdateRatingsJob < ApplicationJob
   queue_as :default
 
   def perform
-    movies = Movie.all
+    movies = Movie.all.order(:id)
     movies.each do |movie|
       movie_data = get_omdb_ratings(movie.imdb_id)
       next if movie_data == "Error"
 
       movie.imdb_rating = (movie_data.imdbRating.to_f * 10).to_i if movie_data.imDbRating != "N/A"
       movie.metacritic_rating = movie_data.Metascore.to_i if movie_data.Metascore != "N/A"
-      movie_data.Ratings.each do |rating|
-        movie.rotten_tomatoes_rating = rating.Value.gsub(/[^0-9]/, '').to_i if rating.Source == "Rotten Tomatoes"
+      unless movie_data.Ratings.nil?
+        movie_data.Ratings.each do |rating|
+          movie.rotten_tomatoes_rating = rating.Value.gsub(/[^0-9]/, '').to_i if rating.Source == "Rotten Tomatoes"
+        end
       end
       if movie_data.Language.split(", ")[0] == "English"
         movie.english = true
