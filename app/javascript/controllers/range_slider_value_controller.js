aad93eb2448839442b40718cac6e5189b4061f35
Jenny Higgins <higgjd@gmail.com>
2022-06-25 16:43:06 +0100

    added imdb as a separate numeric slider with same hiding functionality

--- app/javascript/controllers/range_slider_value_controller.js
+++ app/javascript/controllers/range_slider_value_controller.js
@@ -1,26 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
 
-const slider = document.getElementById("scoreRange");
-const output = document.getElementById("value-output");
+// const slider = document.getElementById("scoreRange");
+// const output = document.getElementById("value-output");
 
 
 export default class extends Controller {
-  static targets = [ "valueOutput", "scoreRange" ]
+  static targets = [ "valueOutput", "percentageScoreRange", "numericScoreRange" ]
 
   connect() {
     // console.log("The 'range slider value' controller is now loaded!")
   }
 
   showScore(event) {
-    // console.log(event)
-    this.valueOutputTarget.innerHTML = `${this.scoreRangeTarget.value}%`
+  if (event.currentTarget === this.percentageScoreRangeTarget) {
+      this.valueOutputTarget.innerHTML = `${this.percentageScoreRangeTarget.value}%`
+  }
 
-    // JAVASCRIPT CODE
-    // output.innerHTML = slider.value; // Display the default slider value
+  if (event.currentTarget === this.numericScoreRangeTarget) {
+    this.valueOutputTarget.innerHTML = `${this.numericScoreRangeTarget.value}%`
+}
 
-    // // Update the current slider value (each time you drag the slider handle)
-    // slider.oninput = function() {
-    //   output.innerHTML = this.value;
     }
 
   showTime(event) {
