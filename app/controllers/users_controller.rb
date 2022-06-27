3cb79c4fad96ed1c445d5d2553aceddae86a1735
Edward Beesley <edwardjbeesley@gmail.com>
2022-06-27 15:11:03 +0100

    added a navbar for the user profile page, which will display on small screens.  Added a method for movie bookmark index, which will show disliked movies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @movies = @user.movies
-    @movie_bookmarks = @user.movie_bookmarks
+    @watchlist = @user.movie_bookmarks.where(bookmark_type: "watchlist")
     @movie_bookmark = MovieBookmark.new
   end
 end
