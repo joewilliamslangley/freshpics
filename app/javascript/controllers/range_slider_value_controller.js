f3c112baa4c278abb19881b7116be2a5b74180db
Edward Beesley <edwardjbeesley@gmail.com>
2022-06-28 22:26:56 +0100

    review score scroller in filters modal now updates when moved

--- app/javascript/controllers/range_slider_value_controller.js
+++ app/javascript/controllers/range_slider_value_controller.js
@@ -5,7 +5,7 @@ import { Controller } from "@hotwired/stimulus"
 
 
 export default class extends Controller {
-  static targets = [ "platform", "valueOutput", "scoreRange", "numericOutput", "percentageOutput","percentageScoreRange", "numericScoreRange" ]
+  static targets = [ "platform", "valueOutput", "scoreRange", "numericOutput", "percentageOutput","percentageScoreRange", "numericScoreRange", "site"]
 
   connect() {
     // console.log("The 'range slider value' controller is now loaded!")
@@ -21,6 +21,19 @@ export default class extends Controller {
 }
 
     }
+
+    showScoreFilter(event) {
+      let input = document.querySelectorAll('#review_site')[1];
+      let platform = input.options[input.selectedIndex].value;
+      console.log(platform)
+    if (platform === "rotten_tomatoes" || platform === "metacritic")  {
+        this.percentageOutputTarget.innerHTML = `${this.percentageScoreRangeTarget.value}%`
+    }
+    if (platform === "imdb") {
+      this.percentageOutputTarget.innerHTML = `${this.percentageScoreRangeTarget.value / 10}`
+  }
+
+      }
 
   showTime(event) {
       if (this.scoreRangeTarget.value > 179) {
