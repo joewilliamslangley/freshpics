2e427af8328a31bc46d9c23ba695a4d627591aa8
Edward Beesley <edwardjbeesley@gmail.com>
2022-06-28 16:05:53 +0100

    amended hide score slider controller so that the visualised score changes between percentage and out of 10 when user selects RT/MC/IMDB

--- app/javascript/controllers/hide_score_slider_controller.js
+++ app/javascript/controllers/hide_score_slider_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 
 export default class extends Controller {
-  static targets = [ "percentageQuestion", "numericQuestion" ]
+  static targets = [ "percentageQuestion", "numericQuestion", "output", "scoreRange" ]
 
   connect() {
     // console.log("The 'hide slider' controller is now loaded!")
@@ -12,15 +12,14 @@ export default class extends Controller {
     if (event.currentTarget.value === "metacritic" || event.currentTarget.value === "imdb" || event.currentTarget.value === "rotten_tomatoes") {
       console.log(event.currentTarget.value)
       this.percentageQuestionTarget.classList.remove("hidden");
+      this.outputTarget.innerHTML = `${this.scoreRangeTarget.value}%`;
       // this.numericQuestionTarget.classList.add("hidden");
     }
 
-    // if (event.currentTarget.value === "imdb") {
-    //   console.log("IMDB selected")
-    //   this.numericQuestionTarget.classList.remove("hidden");
-    //   this.percentageQuestionTarget.classList.add("hidden");
-
-    // }
+    if (event.currentTarget.value === "imdb") {
+      console.log("IMDB selected")
+      this.outputTarget.innerHTML = `${this.scoreRangeTarget.value / 10}`;
+    }
 
     if (event.currentTarget.value === "no_site") {
       console.log("No site selected")
