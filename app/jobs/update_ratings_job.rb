98026040668e1ca4932cf62570a998512566fb86
Edward Beesley <edwardjbeesley@gmail.com>
2022-06-30 13:17:53 +0100

    rake job broke, so amended job to just target movies from the point of break

--- app/jobs/update_ratings_job.rb
+++ app/jobs/update_ratings_job.rb
@@ -8,7 +8,7 @@ class UpdateRatingsJob < ApplicationJob
   queue_as :default
 
   def perform
-    movies = Movie.all.order(:id)
+    movies = Movie.where("id >= ?", 11139).order(:id)
     movies.each do |movie|
       movie_data = get_omdb_ratings(movie.imdb_id)
       next if movie_data == "Error"
